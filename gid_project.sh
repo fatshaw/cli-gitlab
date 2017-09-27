df9b4b8eec57d62d064735df5bc972407157d7de
fatshaw <chjxiao@gmail.com>
2017-09-27 18:57:12 +0800

    Create gid_project.sh

---
+++ gid_project.sh
@@ -0,0 +1,34 @@
+#usage: sh gid_project.sh project_name_filter [gitinspect_options]
+#example: sh gid_project.sh "futures/ios" --since=2017/08/20
+#example: sh gid_project.sh "java-backend/futures" --since=2017/08/20
+#example: sh gid_project.sh "futures/node"
+
+
+if [ $# -lt 1 ];then
+	echo "usage sh gid_project.sh project_name_filter [gitinspect_options]"
+	exit 0
+fi	
+
+echo "fetch projects from gitlab, please wait....." 
+
+projects=`gitlab projects | grep 'http_url_to_repo' | sed -e 's/"http_url_to_repo": "//g' | sed -e 's/",//g' |grep -i -E "$1" |sed 's/ //g'`
+
+for p in $projects
+do
+	name=`echo $p |awk -F '/' '{print $NF}' |sed -n 's/.git//p'` 
+	echo "name="$name
+	if [ -d $name ];then
+		cd $name
+		git pull
+		cd -	
+	else
+		git clone $p
+	fi
+	project=$project" "$name
+done
+
+
+echo "generate project record, please wait ....."
+
+gitinspector $2 --format=html --timeline --localize-output -w $project > project.html
+open project.html
