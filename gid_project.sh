c1c0e894727fa1cfe63b40845eb759359eeb668c
fatshaw <chjxiao@gmail.com>
2017-09-29 11:37:46 +0800

    Update gid_project.sh

--- gid_project.sh
+++ gid_project.sh
@@ -1,3 +1,4 @@
+
 #usage: sh gid_project.sh project_name_filter [gitinspect_options]
 #example: sh gid_project.sh "futures/ios" --since=2017/08/20
 #example: sh gid_project.sh "java-backend/futures" --since=2017/08/20
@@ -11,8 +12,13 @@ fi
 
 echo "fetch projects from gitlab, please wait....." 
 
-projects=`gitlab projects | grep 'http_url_to_repo' | sed -e 's/"http_url_to_repo": "//g' | sed -e 's/",//g' |grep -i -E "$1" |sed 's/ //g'`
+projects=`gitlab projects | grep 'ssh_url_to_repo' | sed -e 's/"ssh_url_to_repo": "//g' | sed -e 's/",//g' |grep -i -E "$1" |sed 's/ //g'`
 
+
+if [ ! -d output ]; then
+  mkdir output
+fi
+pushd output
 for p in $projects
 do
 	name=`echo $p |awk -F '/' '{print $NF}' |sed -n 's/.git//p'` 
@@ -30,5 +36,8 @@ done
 
 echo "generate project record, please wait ....."
 
-gitinspector $2 --format=html --timeline --localize-output -w $project > project.html
+gitinspector $2 --format=html --timeline --localize-output -w $project > ../project.html
+
+popd
+
 open project.html
